a811b5a6655724beafc2cb54e59026c893df5a6d
thanhtunng98 <tung.vt164571@sis.hust.edu.vn>
2020-03-10 08:54:29 +0700

    week1 commit

---
+++ week1/ex5.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Exercise 5</title>
+</head>
+<body>
+	
+	<div style="width: 100%; display: flex; justify-content: center;">
+		<table border="1">
+			<tr>
+				<th colspan="2">
+					<h3> Thanh Tung Vu</h3>
+					<p>HUST 4th-year student
+					<br> Orientation: Embedded engineer
+					</p>
+				</th>
+				<th>
+					<img style="width:80px; height: 100px" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/71945345_1191011711099176_5347776596155564032_n.jpg?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=s0P7G8Re_X8AX_mDliu&_nc_ht=scontent.fhan2-4.fna&oh=9f579e07bbcdc5d8b488438443c48d48&oe=5E99471F">
+				</th>
+			</tr>
+
+		<tr>
+			<td>Education Process</td>
+			<td>Studying</td>
+		</tr>
+		<tr>
+			<td>Working Process</td>
+			<td>Currently none</td>
+		</tr>
+		<tr>
+			<td>Society activities</td>
+			<td>I-Buddy</td>
+		</tr>
+		<tr>
+			<td>Skills</td>
+			<td>
+				<ul>
+					<li>C</li>
+					<li>UART, I2C</li>
+					<li>Java</li>
+					<li>OSEK OS</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td>Experiences</td>
+			<td>FPT Software</td>
+		</tr>
+		<tr>
+			<td>Certifications</td>
+			<td>
+				<ul>
+					<li>Hackathon Junction X 2018</li>
+					<li>FPT Intership</li>
+					<li>I-Buddy</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td>Hobbies</td>
+			<td>Music, Movies, Racing :))</td>
+		</tr>
+		<tr>
+			<td>Desire</td>
+			<td>Learn more Communication skill, Technique skill</td>
+		</tr>
+		</table>
+	</div>
+</body>
+</html>
