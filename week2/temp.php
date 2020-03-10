5bfc00c99baf3cd6c6b7bb1997cea30f83549031
thanhtunng98 <tung.vt164571@sis.hust.edu.vn>
2020-03-10 09:42:58 +0700

    first commit week2

---
+++ week2/temp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Var example</title>
+</head>
+<body>
+	<?php
+		$first_num = 1;	$sec_num = 2;
+		$temp = $first_num; $diff = $temp-$sec_num;
+		print("sum : $temp+$sec_num <br>
+				diff: $diff");
+
+		$banana1 = "banana"; $banana2 = "and banana";
+		print("<br> $banana1 + $banana2");
+		
+		$first_name = "Thanh Tung"; $family_name = "Vu";
+		$full_name = $first_name.$family_name;
+		print("<br>$full_name");
+	?>
+</body>
+</html>
