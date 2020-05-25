c7053fefe502a5b0874ff2bc01adb136d43d1e71
= <thanhtungvu.hust@gmail.com>
2020-05-25 21:32:22 +0700

    lab10

---
+++ week10/lab10/mvc-framework/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+define('DEVELOPMENT_ENVIRONMENT', true);
+
+define('DB_NAME', 'mvc-framework');
+define('DB_USER', 'thanhtunng');
+define('DB_PASSWORD', 'thanhtung98');
+define('DB_HOST', 'localhost:6969');
